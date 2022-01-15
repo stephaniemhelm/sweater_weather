0db505547762d13c747f4e848a0ca470866c31c9
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-15 13:35:45 -0700

    current weather poro

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -13,14 +13,18 @@ class CurrentWeather
 
   def initialize(data)
     @datetime = Time.at(data[:current][:dt]).to_s
-    @sunrise = Time.at(data[:current][:sunrise])
-    @sunset = Time.at(data[:current][:sunset])
-    @temperature = data[:current][:temp]
-    @feels_like = data[:current][:feels_like]
+    @sunrise = Time.at(data[:current][:sunrise]).to_s
+    @sunset = Time.at(data[:current][:sunset]).to_s
+    @temperature = kelvin_to_farenheit(data[:current][:temp])
+    @feels_like = kelvin_to_farenheit(data[:current][:feels_like])
     @humidity = data[:current][:humidity]
     @uvi = data[:current][:uvi]
     @visibility = data[:current][:visibility]
     @conditions = data[:current][:weather].first[:main]
     @icon = data[:current][:weather].first[:icon]
+  end
+
+  def kelvin_to_farenheit(temperature)
+    ((temperature - 273.15) * 1.8 + 32).round(2)
   end
 end
--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe WeatherFacade do
-  it 'can get current weather for a location' do
-    latitude = 38.841772
-    longitude = -106.132562
-
-    result = WeatherFacade.get_current_weather(latitude, longitude)
-
-    expect(result).to be_a CurrentWeather
-  end
+RSpec.describe CurrentWeather do
 
   it 'has current weather attributes' do
     data = {:lat=>38.8418,
