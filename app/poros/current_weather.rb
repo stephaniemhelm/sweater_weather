0f27006cb436111f84d88075770dc58439d7a416
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-15 09:43:31 -0700

    weather specs

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -1,7 +1,26 @@
 class CurrentWeather
 
+  attr_reader :datetime,
+              :sunrise,
+              :sunset,
+              :temperature,
+              :feels_like,
+              :humidity,
+              :uvi,
+              :visibility,
+              :conditions,
+              :icon
+
   def initialize(data)
-
-
-  end 
+    @datetime = Time.at(data[:current][:dt]).to_s
+    @sunrise = Time.at(data[:current][:sunrise])
+    @sunset = Time.at(data[:current][:sunset])
+    @temperature = data[:current][:temp]
+    @feels_like = data[:current][:feels_like]
+    @humidity = data[:current][:humidity]
+    @uvi = data[:current][:uvi]
+    @visibility = data[:current][:visibility]
+    @conditions = data[:current][:weather].first[:main]
+    @icon = data[:current][:weather].first[:icon]
+  end
 end
