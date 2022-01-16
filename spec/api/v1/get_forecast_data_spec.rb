939a833df4201ffd592ce97892a6b0cfc9089da1
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-15 21:15:03 -0700

    coverage

--- spec/api/v1/get_forecast_data_spec.rb
+++ spec/api/v1/get_forecast_data_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Get /api/v1/forecast' do
 
     expect(response).to be_successful
 
+    forecast_response = JSON.parse(response.body, symbolize_names: true)
 
+    expect(forecast_response).to be_a Hash
+    expect(forecast_response[:data]).to be_a Hash
+    expect(forecast_response[:data][:type]).to eq('forecast')
+    expect(forecast_response[:data]).to have_key :attributes
+    expect(forecast_response[:data][:attributes]).to have_key :current_weather 
+    expect(forecast_response[:data][:attributes]).to have_key :daily_weather
+    expect(forecast_response[:data][:attributes]).to have_key :hourly_weather
   end
 end
