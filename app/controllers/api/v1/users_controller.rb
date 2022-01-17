594310cf28e2d62537b4c6ebeaf40b441515328f
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-17 10:48:50 -0700

    sessions controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
       if user.password != user.password_confirmation
         render json: { error: 'Passwords do not match.' }, status: 400
       elsif User.find_by(email: user[:email]).present?
-        render json: { error: 'Email has already been taken' }, status: 400
+        render json: { error: 'Email has already been taken.' }, status: 400
       else
         user.save
         user.update(api_key: SecureRandom.hex)
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#show'
-      get '/image', to: 'image#show'
-      post '/users', to: 'users#create'
+      get '/forecast',      to: 'forecast#show'
+      get '/image',         to: 'image#show'
+      post '/users',        to: 'users#create'
+      post '/sessions',     to: 'sessions#create'
     end
   end
 end
