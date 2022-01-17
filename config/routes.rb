b7d82694a20994f2870fe179f07543ddf5f3503d
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-17 08:43:02 -0700

    refactor users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/image', to: 'image#show'
-      #get '/users/new', to: 'users#new'
       post '/users', to: 'users#create'
     end
   end
